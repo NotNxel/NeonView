ff33f1010f0b630f40463730d3fb809ce1175730
Neel Nikhil <notnxel@gmail.com>
2025-10-26 23:52:46 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
-
-
-// Your new showsDb dataset integrated below
+// ✅ Final Expanded showsDb Dataset
 const showsDb = [
+  // ---------------------------
+  // Existing Entries
+  // ---------------------------
   {
     title: 'Dexter',
     poster: 'https://resizing.flixster.com/-XZAfHZM39UwaGJIFWKAE8fS0ak=/v3/t/assets/p185267_b_v8_ac.jpg',
@@ -100,11 +101,173 @@ const showsDb = [
     actors: 'Jared Padalecki, Jensen Ackles',
     country: 'US',
     desc: 'Brothers hunt supernatural creatures while facing dark forces.'
+  },
+
+  // ---------------------------
+  // Added Movie Series
+  // ---------------------------
+  {
+    title: 'Batman Begins',
+    poster: 'https://resizing.flixster.com/jO-Hz6OiF3avhyVZrmp3Xz9Mm3E=/v3/t/assets/p34603_p_v13_ab.jpg',
+    genre: 'Action, Crime, Drama',
+    director: 'Christopher Nolan',
+    actors: 'Christian Bale',
+    country: 'US',
+    desc: 'Bruce Wayne becomes Batman to fight the forces threatening Gotham City.'
+  },
+  {
+    title: 'The Dark Knight',
+    poster: 'https://resizing.flixster.com/t55vT4wZy0BnwwCBkA9kavJc0IA=/v3/t/assets/p173378_p_v13_au.jpg',
+    genre: 'Action, Crime, Drama, Thriller',
+    director: 'Christopher Nolan',
+    actors: 'Christian Bale, Heath Ledger',
+    country: 'US',
+    desc: 'Batman faces chaos unleashed by the Joker in Gotham City.'
+  },
+  {
+    title: 'The Dark Knight Rises',
+    poster: 'https://resizing.flixster.com/KoRT6DyD49Afg7LjU22nBvE5I6A=/v3/t/assets/p9281323_p_v13_ah.jpg',
+    genre: 'Action, Crime, Drama, Thriller',
+    director: 'Christopher Nolan',
+    actors: 'Christian Bale, Tom Hardy, Anne Hathaway',
+    country: 'US',
+    desc: 'A reclusive Bruce Wayne returns to defend Gotham from Bane.'
+  },
+  {
+    title: 'The Matrix',
+    poster: 'https://resizing.flixster.com/qSxWbP_mgMqRn_nt4FLIUPBPk5I=/v3/t/assets/p22804_p_v13_af.jpg',
+    genre: 'Action, Sci-Fi',
+    director: 'Lana Wachowski, Lilly Wachowski',
+    actors: 'Keanu Reeves, Carrie-Anne Moss, Laurence Fishburne',
+    country: 'US',
+    desc: 'A hacker discovers a dystopian world controlled by machines.'
+  },
+  {
+    title: 'The Matrix Reloaded',
+    poster: 'https://resizing.flixster.com/SSGAwCbeYMSwAf5sXCeU-hgeKWg=/v3/t/assets/p31956_p_v13_ao.jpg',
+    genre: 'Action, Sci-Fi',
+    director: 'Lana Wachowski, Lilly Wachowski',
+    actors: 'Keanu Reeves, Laurence Fishburne, Carrie-Anne Moss',
+    country: 'US',
+    desc: 'Neo continues his battle against the machines as he learns more about the Matrix.'
+  },
+  {
+    title: 'The Matrix Revolutions',
+    poster: 'https://resizing.flixster.com/zEa3jOsbElAZf1H1V2v9Rk17sHk=/v3/t/assets/p31955_p_v13_ag.jpg',
+    genre: 'Action, Sci-Fi',
+    director: 'Lana Wachowski, Lilly Wachowski',
+    actors: 'Keanu Reeves, Laurence Fishburne, Carrie-Anne Moss',
+    country: 'US',
+    desc: 'Human and machine forces face their final battle for survival.'
+  },
+  {
+    title: 'Iron Man',
+    poster: 'https://resizing.flixster.com/uIApBKUhGIxe7sDnk-_6DqkHCAc=/v3/t/assets/p170620_p_v13_at.jpg',
+    genre: 'Action, Adventure, Sci-Fi',
+    director: 'Jon Favreau',
+    actors: 'Robert Downey Jr., Gwyneth Paltrow, Jeff Bridges',
+    country: 'US',
+    desc: 'Tony Stark builds the Iron Man suit to fight evil after being captured.'
+  },
+  {
+    title: 'Iron Man 2',
+    poster: 'https://resizing.flixster.com/7L7HXz9nBZV5xvXGkGZqcup5m5Y=/v3/t/assets/p7825626_p_v13_af.jpg',
+    genre: 'Action, Adventure, Sci-Fi',
+    director: 'Jon Favreau',
+    actors: 'Robert Downey Jr., Scarlett Johansson, Mickey Rourke',
+    country: 'US',
+    desc: 'Tony Stark faces new enemies and rivals while the world learns about Iron Man.'
+  },
+  {
+    title: 'Iron Man 3',
+    poster: 'https://resizing.flixster.com/M1s3VEoFXBIzQ5_V7kDYRQCozgg=/v3/t/assets/p9249673_p_v13_ar.jpg',
+    genre: 'Action, Adventure, Sci-Fi',
+    director: 'Shane Black',
+    actors: 'Robert Downey Jr., Gwyneth Paltrow, Ben Kingsley',
+    country: 'US',
+    desc: 'After the events of Avengers, Tony Stark faces a powerful new foe: the Mandarin.'
+  },
+  {
+    title: 'Avengers: Endgame',
+    poster: 'https://resizing.flixster.com/jxZ0hPTvZCPspRP7SoFZP0wHRkE=/v3/t/assets/p17062095_p_v13_aa.jpg',
+    genre: 'Action, Adventure, Sci-Fi',
+    director: 'Anthony Russo, Joe Russo',
+    actors: 'Robert Downey Jr., Chris Evans, Scarlett Johansson',
+    country: 'US',
+    desc: 'The Avengers unite to undo Thanos’s devastating snap.'
+  },
+  {
+    title: 'Spider-Man: No Way Home',
+    poster: 'https://resizing.flixster.com/uXw47YN0rbcoXIg5pBifDQAS6sw=/v3/t/assets/p21584325_p_v13_ab.jpg',
+    genre: 'Action, Adventure, Sci-Fi',
+    director: 'Jon Watts',
+    actors: 'Tom Holland, Zendaya, Benedict Cumberbatch',
+    country: 'US',
+    desc: 'Spider-Man faces multiverse villains while confronting his own identity.'
+  },
+
+  // ---------------------------
+  // Global Popular Shows
+  // ---------------------------
+  {
+    title: 'Money Heist',
+    poster: 'https://resizing.flixster.com/vWBYMPe0RZpq-FKFpP2s9vRzlNw=/v3/t/assets/p16179980_b_v13_aa.jpg',
+    genre: 'Action, Crime, Thriller',
+    director: 'Álex Pina',
+    actors: 'Úrsula Corberó, Álvaro Morte',
+    country: 'ES',
+    desc: 'A criminal mastermind plans the biggest heist in Spain’s history.'
+  },
+  {
+    title: 'Prison Break',
+    poster: 'https://resizing.flixster.com/oNKZFWaU0p6d9INcOTuDh50wrSY=/v3/t/assets/p185054_b_v8_aa.jpg',
+    genre: 'Action, Crime, Drama, Thriller',
+    director: 'Paul Scheuring',
+    actors: 'Wentworth Miller, Dominic Purcell',
+    country: 'US',
+    desc: 'A man intentionally gets imprisoned to help his brother escape death row.'
+  },
+  {
+    title: 'Friends',
+    poster: 'https://resizing.flixster.com/LfZnVBRZpLi6B4xANzqxFvFG9h0=/v3/t/assets/p184585_b_v8_aa.jpg',
+    genre: 'Comedy, Romance',
+    director: 'David Crane, Marta Kauffman',
+    actors: 'Jennifer Aniston, Courteney Cox, Matthew Perry',
+    country: 'US',
+    desc: 'Six friends navigate life and relationships in New York City.'
+  },
+  {
+    title: 'Moon Knight',
+    poster: 'https://resizing.flixster.com/qcTD1V1rbcU7ZRNAoWq5YB0uu5I=/v3/t/assets/p23026432_b_v13_ac.jpg',
+    genre: 'Action, Adventure, Fantasy, Superhero',
+    director: 'Mohamed Diab',
+    actors: 'Oscar Isaac, Ethan Hawke',
+    country: 'US',
+    desc: 'A man battles dissociative identity disorder and confronts Egyptian gods.'
+  },
+  {
+    title: 'Loki',
+    poster: 'https://resizing.flixster.com/SFSACkYaZXoqjtm6o2ekltlbKzA=/v3/t/assets/p22324440_b_v13_aa.jpg',
+    genre: 'Action, Fantasy, Sci-Fi',
+    director: 'Kate Herron',
+    actors: 'Tom Hiddleston, Owen Wilson',
+    country: 'US',
+    desc: 'The God of Mischief navigates the chaos of the multiverse.'
+  },
+  {
+    title: 'The Mandalorian',
+    poster: 'https://resizing.flixster.com/iuJtsQyR9yBpCC8fLBfwq8uJmDE=/v3/t/assets/p16927671_b_v13_ac.jpg',
+    genre: 'Action, Adventure, Sci-Fi',
+    director: 'Jon Favreau',
+    actors: 'Pedro Pascal',
+    country: 'US',
+    desc: 'A lone bounty hunter explores the outer reaches of the galaxy.'
   }
 ];
 
-// Combine moviesDb and showsDb into one for search and recommendation (optional)
+// ✅ Combined DB for search and recommendations
 const combinedDb = [...showsDb];
+
 
 // Case-insensitive fuzzy match function adapted for combinedDb (use whichever dataset you want)
 function autoSuggest(query) {
